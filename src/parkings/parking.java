00d4db95674d315130c08e1674afce8e793cec40
vadim lutski <vadim.lutski.a@gmail.com>
2023-11-12 14:38:08 +0300

    first

---
+++ src/parkings/parking.java
@@ -0,0 +1,151 @@
+package parkings;
+
+
+import java.io.Serializable;
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.io.*;
+import java.util.*;
+import java.time.*;
+
+public class parking implements Serializable
+{
+    private static final long serialVersionUID = 1L;
+    String number;
+    public static final String car_number = "number";
+    String owner;
+    public static final String car_owner = "owner";
+    Date startdate = new Date();
+    public static final String start_date = "start_date";
+    static final SimpleDateFormat format = new  SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    Date enddate = new Date();
+
+    String dur ;
+    public static final String Dur = "Park_time";
+    public static final String end_date = "end_date";
+
+    double price;
+    public static final String P_price = "Price";
+
+
+
+    public static final String AREA_DEL = "\n";
+
+    public String getStrISBN() {
+        return number;
+    }
+
+    public final void setStrnum(String strnum) {
+        if (strnum == null || strnum.isEmpty()) {
+            throw new IllegalArgumentException("Illegal number");
+        }
+        this.number = strnum;
+    }
+
+    public String getowner() {
+        return owner;
+    }
+
+    public final void setowner(String owner) {
+        if (owner == null || owner.isEmpty()) {
+            throw new IllegalArgumentException("Illegal owner");
+        }
+        this.owner = owner;
+    }
+
+
+    public double getPrice() {
+        return price;
+    }
+
+    public final void setPrice(String strPrice) {
+        boolean isError = false;
+        double p = 0;
+        try {
+            p = Double.parseDouble(strPrice);
+        } catch (Error | Exception e) {
+            isError = true;
+        }
+        if (isError || p <= 0) {
+            throw new IllegalArgumentException("Illegal price");
+        }
+        this.price = p;
+    }
+
+    String getparktime()
+    {
+        long difference_In_Time = enddate.getTime() - startdate.getTime();
+        dur ="" + difference_In_Time;
+        return dur;
+    }
+
+    public final void setend_datepark(String endpark) throws ParseException {
+        this.enddate=format.parse(endpark);
+    }
+
+    public final void setstart_datepark(String startpark) throws ParseException {
+        this.startdate=format.parse(startpark);
+    }
+
+
+
+
+    public static boolean nextRead(Scanner fin, PrintStream out) {
+        return nextRead(car_number, fin, out);
+    }
+
+    static boolean nextRead(final String prompt, Scanner fin, PrintStream out) {
+        out.print(prompt);
+        out.print(": ");
+        return fin.hasNextLine();
+    }
+
+    public static final String authorDel = ",";
+
+    public static parking read(Scanner fin, PrintStream out) throws IOException,
+            NumberFormatException, ParseException {
+        String str;
+        parking par = new parking();
+        par.number = fin.nextLine().trim();
+
+        if (!nextRead(car_owner, fin, out)) {
+            return null;
+        }
+        par.owner = fin.nextLine();
+        if (!nextRead(start_date, fin, out)) {
+            return null;
+        }
+
+        par.startdate=format.parse(fin.nextLine());
+
+        if (!nextRead(end_date, fin, out)) {
+            return null;
+        }
+        par.enddate=format.parse(fin.nextLine());
+
+
+        if (!nextRead(P_price, fin, out)) {
+            return null;
+        }
+        str = fin.nextLine();
+        par.price = Double.parseDouble(str);
+        return par;
+    }
+
+    public parking() {
+    }
+
+    public static final String areaDel = "\n";
+
+    public String toString() {
+        return new String(
+                number + areaDel +
+                        owner + areaDel +
+                        startdate + areaDel +
+                        enddate+ areaDel +
+                        price
+        );
+    }
+}
